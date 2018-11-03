d424fcba2a089a740693427419a9524497d08b57
lourod1987 <louart87@gmail.com>
2018-11-03 13:50:32 -0500

    initial project setup based off of previously built components

--- ask-libbie-frontend/src/App.js
+++ ask-libbie-frontend/src/App.js
@@ -1,28 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { load_google_maps } from './GoogleMapsUtility'
+import Map from './Map';
+import Header from './Header';
+import Sidebar from './Sidebar';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        query: '',
+        locations: [],
+        menu: false
+    }
+
+    componentDidMount() {
+        let googleMapsPromise = load_google_maps();
+
+        Promise.all([
+        googleMapsPromise
+        ])
+        .then( values => {
+            let google = values[0];
+
+            this.google = google;
+            this.markers = [];
+
+            this.map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 10,
+                center: {lat: 29.760427, lng: -95.369803},
+                scrollwheel: true
+            });
+        })
+
+    }
+
+     //show or hide the menu
+    toggleMenu = () => {
+        const map = document.getElementById('map');
+        
+        //add and remove classes that style the map according to whether or not the menu is displayed
+        if (this.state.filterMenu === true) {
+            map.classList.add('map');
+            map.classList.remove('map-sidebar');
+        } else {
+            map.classList.remove('map');
+            map.classList.add('map-sidebar');
+        }
+
+        //change state of filterMenu boolean based on it's current state which toggles menu rendering
+        return this.state.menu === true ?
+            this.setState({ menu: false }) :
+            this.setState({ menu: true });
+    }
+
+    render() {
+        const { query, menu } = this.state;
+        return (
+            <div className="App">
+                {/* <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                </header> */}
+                <Header toggleMenu={this.toggleMenu} />
+                <main>
+                    <Map />
+                    { menu === true &&
+                        <Sidebar
+                            query={query}
+                            // filter={ evt => {this.filter(evt.target.value)} }
+                            // filteredPlaces={filteredPlaces}
+                            // listSelect={this.listSelect}
+                        />
+                    }
+                </main>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ ask-libbie-frontend/src/GoogleMapsUtility.js
@@ -0,0 +1,21 @@
+//Special thanks to Ryan Waite for this helper function
+export function load_google_maps() {
+    return new Promise(function(resolve, reject) {
+      // define the global callback that will run when google maps is loaded
+      window.resolveGoogleMapsPromise = function() {
+        // resolve the google object
+        resolve(window.google);
+        // delete the global callback to tidy up since it is no longer needed
+        delete window.resolveGoogleMapsPromise;
+      }
+      // Now, Load the Google Maps API
+      const script = document.createElement("script");
+      const API_KEY = 'AIzaSyCMYnXvS0hKPei5yYFHBRjzMQLw-o8i4Mg';
+      script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
+      script.async = true;
+      document.body.appendChild(script);
+    })
+    .catch( error => { 
+      console.log(error);
+    });
+  }
