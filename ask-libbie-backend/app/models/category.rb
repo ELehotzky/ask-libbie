73b722ede0ba12420c0684d2456f3f23d00cbdd2
Marylene Chan <marylene.chan@gmail.com>
2018-11-03 14:57:59 -0500

    update model and add seed data

--- ask-libbie-backend/app/models/category.rb
+++ ask-libbie-backend/app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-	
-	has_many: :resource_categories
-	has_many: :resources, through: :resource_categories
+
+	has_many :resource_categories
+	has_many :resources, through: :resource_categories
 
 end
