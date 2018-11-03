a078f81ed62719a8c67a2c38ba2850d0e7b245bb
ELehotzky <Lehotzky.Erica@gmail.com>
2018-11-03 13:40:35 -0500

    started seed file

--- ask-libbie-backend/db/schema.rb
+++ ask-libbie-backend/db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 2018_11_03_175338) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
