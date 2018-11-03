6b9ffcf7de2ae6032c29d64ee6df1525d3bbfcd1
ELehotzky <Lehotzky.Erica@gmail.com>
2018-11-03 13:08:31 -0500

    fixed models and migrations

---
+++ ask-libbie-backend/db/migrate/20181103175158_create_resources.rb
@@ -0,0 +1,17 @@
+class CreateResources < ActiveRecord::Migration[5.2]
+  def change
+    create_table :resources do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.integer :zipcode
+      t.string :website
+      t.string :description
+      t.string :phone
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
---
+++ ask-libbie-backend/db/schema.rb
@@ -0,0 +1,50 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_11_03_175338) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "resource_categories", force: :cascade do |t|
+    t.bigint "resource_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_resource_categories_on_category_id"
+    t.index ["resource_id"], name: "index_resource_categories_on_resource_id"
+  end
+
+  create_table "resources", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.integer "zipcode"
+    t.string "website"
+    t.string "description"
+    t.string "phone"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "resource_categories", "categories"
+  add_foreign_key "resource_categories", "resources"
+end
