8814f0d233e73d6fd7ebba6044e827475812a446
lourod1987 <louart87@gmail.com>
2018-11-04 07:03:40 -0600

    changing header to be appropriate for form pages

--- ask-libbie-frontend/src/Header.js
+++ ask-libbie-frontend/src/Header.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import menu from './menu.svg';
+// import { Link } from 'react-router-dom';
+// import menu from './menu.svg';
 import PropTypes from 'prop-types';
 
 export class Header extends Component {
-    static propTypes = {
-        toggleMenu: PropTypes.func.isRequired
-    }
-
+    // static propTypes = {
+    //     toggleMenu: PropTypes.func.isRequired
+    // }
   render() {
-    const { toggleMenu } = this.props;
     return (
         <header>
-          <button aria-label="Menu" className="menu-button" onClick={() => toggleMenu()}>
-          <div className="menuImg-credit">
-            Icons made by 
-            <a href="https://www.flaticon.com/authors/cole-bemis" title="Cole Bemis">Cole Bemis</a> 
-            from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> 
-            is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a>
-          </div>
-            <img src={menu} className="menu-icon" alt=""></img>
-          </button>
+          {/* <Link to="/"> */}
+              <a href="http://127.0.0.1:3000/">Back</a>
+            {/* <button aria-label="Back" className="menu-button">
+              <img src={menu} className="menu-icon" alt=""></img> 
+            </button> */}
+          {/* </Link> */}
           <h1>Ask Libbie</h1>
         </header>
     );
